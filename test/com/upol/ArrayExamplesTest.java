0bd6351e985cc4d85bc1a3b12172242c999bc8f2
Muhammad Hasan <mhasan@opentext.com>
2020-04-11 11:54:33 +0530

    Update 2

--- test/com/upol/ArrayExamplesTest.java
+++ test/com/upol/ArrayExamplesTest.java
@@ -1,24 +1,60 @@
 package com.upol;
 
+
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ArrayExamplesTest {
+
+    ArrayUtil arrayUtil = new ArrayUtil();
+
+/*
+    @BeforeClass
+    void setUp(){
+        ArrayExamples arrayExamples = new ArrayExamples();
+    }
+*/
 
     @Test
     void sortIntArr() {
 
         int[] intArr = {5, 6, 1, 8, 3, 4};
-        ArrayExamples arrayExamples = new ArrayExamples(intArr);
-        intArr = arrayExamples.sortIntArr();
+
+        intArr = arrayUtil.sortIntArr(intArr);
 
         //Arrays.asList(intArr).stream().map(e->Integer.valueOf(e) ).forEach(e->System.out.println(e));
+        arrayUtil.printArray(intArr);
 
-        for(int elem: intArr){
-            System.out.println(elem);
-        }
     }
+
+    @Test
+    void testDeleteElement(){
+
+        int[] intArr = {5, 6, 1, 8, 3, 4};
+
+        int[] arrayAfterDelete = arrayUtil.deleteElementByIndex(5, intArr);
+        arrayUtil.printArray(arrayAfterDelete);
+
+    }
+
+    @Test
+    void testDeleteElementException(){
+
+        int[] intArr = {5, 6, 1, 8, 3, 4};
+
+        assertThrows(ArrayIndexOutOfBoundsException.class, () ->{
+            int[] arrayAfterDelete = arrayUtil.deleteElementByIndex(-9, intArr);
+        });
+    }
+
+    @Test
+    void testRemoveDuplicates(){
+
+        int[] intArr = {5, 6, 1, 4, 3, 4};
+        int[] uniqueArray = arrayUtil.removeDuplicates(intArr);
+        arrayUtil.printArray(uniqueArray);
+    }
+
 }
